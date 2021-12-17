81d0c8072a0adf8fa5ad18cb113146dbe156d292
wadwings <158802567@qq.com>
2021-12-17 18:44:47 +0800

    fix: 🐛 fix for dep

--- frontEnd/src/styles/login/login.ts
+++ frontEnd/src/styles/login/login.ts
@@ -1,4 +1,4 @@
-import { makeStyles } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import color from "@styles/color/color";
 
 export default (isWidthLimited: boolean): any => makeStyles(() => ({
